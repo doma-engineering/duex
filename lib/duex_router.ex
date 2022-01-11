83d9b8b6a2aba6fbb38178e51d0d00a13041cdc3
conflagrate <jm@memorici.de>
2022-01-11 20:59:21 +0000

    Problem: No way to work with the multiple files / dirs in browser
    Solution:
    
     - Add HTML and JS that shows modern file management utilities ( thanks, @radivarig )
     - Test it in Firefox
    
    Further work:
    
     - Make an Elixir endpoint which accepts files
     - Figure out why the ZIP turns out empty in FF: https://i.imgur.com/jvfkAJP.png

--- lib/duex_router.ex
+++ lib/duex_router.ex
@@ -10,6 +10,29 @@ defmodule DuexRouter do
   forward("/human", to: Duex.RouterHuman)
   forward("/user", to: Duex.RouterUser)
 
+  @spec priv :: binary()
+  def priv, do: Application.app_dir(:duex, "priv")
+
+  @spec index_html :: binary()
+  def index_html, do: priv() <> "/index.html"
+
+  @spec index_js :: binary()
+  def index_js, do: priv() <> "/index.js"
+
+  match "/" do
+    send_resp(conn, 200, File.read!(index_html()))
+  end
+
+  match "/index.html" do
+    send_resp(conn, 200, File.read!(index_html()))
+  end
+
+  match "/index.js" do
+    conn
+    |> put_resp_content_type("application/javascript")
+    |> send_resp(200, File.read!(index_js()))
+  end
+
   match _ do
     send_resp(
       conn,
---
+++ priv/index.js
@@ -0,0 +1,129 @@
+// TODO: test cross platform support
+// WARNING: https://developer.mozilla.org/en-US/docs/Web/API/DataTransferItem/webkitGetAsEntry
+// TODO: test large amount of files
+// TODO: test error handling
+// TODO: test file name correctness with different selection types
+//       (only folder, only files, files and folders, etc.)
+
+var inputFilesEl = document.getElementById('__input_files')
+var inputFolderEl = document.getElementById('__input_folder')
+var dropzoneEl = document.getElementById('__dropzone')
+var filesEl = document.getElementById('__files')
+
+let state = { files: [] }
+const getState = () => state
+const setFiles = (files) => state.files = files
+
+var rpath = (file) => {
+  if ('custom_relativePath' in file) {
+    return file.custom_relativePath
+  } else {
+    return file.webkitRelativePath
+  }
+}
+
+const saveAsZip = async (event) => {
+  event.preventDefault()
+
+  const zip = new JSZip()
+  const folder = zip.folder()
+
+  const files = getState().files
+  if (!files.length) {
+    alert('no files added, nothing to zip')
+    return
+  }
+  files.forEach(file => folder.file(file.custom_relativePath, file))
+
+  console.log(files[0])
+  // NOTE: paths here are not os specific, safe to use '/'
+  const dirName = rpath(files[0]).split('/')[0]
+  zip.generateAsync({ type: "blob" })
+    .then((content) => saveAs(content, `${dirName}.zip`))
+}
+
+// NOTE: in depth explanation https://stackoverflow.com/a/53058574/3754246
+const getAllFileEntries = async (dataTransferItemList) => {
+  dataTransferItemList = Array.from(dataTransferItemList) // make iterable
+
+  const fileEntries = []
+  const queue = [] // Use BFS to traverse entire directory/file structure
+
+  dataTransferItemList.forEach(item => queue.push(item.webkitGetAsEntry()))
+
+  while (queue.length > 0) {
+    const entry = queue.shift()
+    if (entry.isFile) {
+      fileEntries.push(entry)
+    }
+    else if (entry.isDirectory) {
+      queue.push(...await readAllDirectoryEntries(entry.createReader()))
+    }
+  }
+  return fileEntries
+}
+
+const readAllDirectoryEntries = async (directoryReader) => {
+  const entries = []
+  let readEntries = await readEntriesPromise(directoryReader)
+  while (readEntries.length > 0) {
+    entries.push(...readEntries)
+    readEntries = await readEntriesPromise(directoryReader)
+  }
+  return entries
+}
+
+const readEntriesPromise = async (directoryReader) => {
+  try {
+    return await new Promise((resolve, reject) => {
+      // readEntry returns at most 100 entries, call it repeatedly
+      directoryReader.readEntries(resolve, reject)
+    })
+  }
+  catch (err) { console.log(err) }
+}
+
+const handleFilesChange = (files) => {
+  setFiles(files)
+  filesEl.innerHTML = getState().files.map(file => rpath(file)).join('<br/>')
+}
+
+// needed to prevent drag and drop from opening content in new tab
+dropzoneEl.addEventListener('dragover', (event) => event.preventDefault())
+
+dropzoneEl.addEventListener('drop', async (event) => {
+  event.preventDefault()
+  const entries = [...(await getAllFileEntries(event.dataTransfer.items))]
+  const files = await Promise.all(
+    entries.map(entry => new Promise((resolve, reject) => {
+      entry.file((file) => {
+        // NOTE: file.webkitRelativePath is empty but setting it does nothing
+        // NOTE: mutation
+        rpath(file) = entry.fullPath.slice(1) // remove first '/'
+        resolve(file)
+      })
+    }))
+  )
+  handleFilesChange(files)
+  // cleanup
+  event.dataTransfer.clearData()
+})
+
+inputFolderEl.addEventListener('change', (event) => {
+  event.preventDefault()
+  handleFilesChange([...event.target.files])
+  // cleanup
+  event.target.files = null
+})
+
+inputFilesEl.addEventListener('change', (event) => {
+  event.preventDefault()
+  const files = [...event.target.files]
+  files.forEach(file => {
+    // NOTE: mutation
+    rpath(file) = file.webkitRelativePath ? file.webkitRelativePath : file.name
+  })
+  handleFilesChange(files)
+  // cleanup
+  event.target.files = null
+})
